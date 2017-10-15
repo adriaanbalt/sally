99e2ab46b7498b213ec7d3735bc6bae6690b4453
adriaanbalt <adriaan@balt.us>
2017-10-15 13:17:00 -0400

    routes are now inside of index.js instead of APP.js (which has been deleted)

--- source/index.js
+++ source/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { Route, Link } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './Store'
-import App from './app'
+import Nav from 'components/nav'
+import Footer from 'components/footer'
+import Market from './market'
+import About from './about'
 
 import './index.scss'
 
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
+      <main>
+        <div className="wrapper">
+          <Nav />
+          <Footer />
+          <main>
+            <Route exact path="/" component={Market} />
+            <Route exact path="/about-us" component={About} />
+          </main>
+        </div>
+      </main>
     </ConnectedRouter>
   </Provider>,
   document.querySelector('#root')
