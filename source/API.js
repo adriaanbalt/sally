dca3dc185ad76fe422ac74e6ede88bd70bbddfb2
adriaanbalt <adriaan@balt.us>
2017-10-16 00:14:14 -0400

    API loads the requests

--- source/API.js
+++ source/API.js
@@ -1,17 +1,18 @@
 import request from 'superagent';
 import Promise from 'bluebird';
-import { API, API_VERSION } from './constants'
+import Constants from './constants'
 
 // console.log ( "Constants", API, API_VERSION )
-const apiRequest = o => async (dispatch, getState) => {
-  console.log( 'apiRequest', o )
+const apiRequest = o => {
+  let endpoint = `${Constants.API}/${Constants.API_VERSION}${o.route}`
+  console.log( 'apiRequest', o)
   o.params = o.params || [];
   //if(o.body && ['get','del'].indexOf(o.method)!==-1) throw new Error("get and delete requests can't have a body with superagent library");
   o.body = o.body || {};
   o.headers = o.headers || {'Accept': 'application/json'}; // default to 'Accept': 'application/json' header
   return new Promise( (resolve,reject) => {
     request
-    [o.method]( `${Constants.API}/${Constants.API_VERSION}${o.route}` ) // if o.params is not string destructure it
+    [o.verb.toLowerCase()]( endpoint )
     .set(o.headers)
     .send(o.body)
     .end((err, res) => {
