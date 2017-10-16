9d41df1d19e45eef742ce01975318fff5de32a47
adriaanbalt <adriaan@balt.us>
2017-10-16 16:02:02 -0400

    updaitng routes and including Switch

--- source/index.js
+++ source/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Route, Link } from 'react-router-dom'
+import { Switch, Route } from 'react-router'
+import { Link } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './Store'
-import Nav from 'components/nav'
-import Footer from 'components/footer'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
 import Market from './market'
 import About from './about'
-import Details from './exchanges/details'
+import Details from './details'
+import NotFound from './NotFound'
 
 import './index.scss'
 
@@ -17,13 +19,16 @@ render(
     <ConnectedRouter history={history}>
       <main>
         <div className="wrapper">
-          <Nav />
-          <Footer />
+          <Header />
           <main>
-            <Route path="/" component={Market} />
-            <Route path="/about" component={About} />
-            <Route path="/:symbol" component={Details} />
+            <Switch>
+              <Route exact path="/" component={Market} />
+              <Route path="/about" component={About} />
+              <Route path="/:symbol" component={Details} />
+              <Route component={NotFound} />
+            </Switch>
           </main>
+          <Footer />
         </div>
       </main>
     </ConnectedRouter>
