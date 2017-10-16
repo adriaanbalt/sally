07e3208450006b83cbdb3c3da5aa33612e8ffc01
adriaanbalt <adriaan@balt.us>
2017-10-16 18:25:46 -0400

    loadings the correct pages

--- source/index.js
+++ source/index.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { Switch, Route } from 'react-router'
+import { Link } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './Store'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
 import App from './App'
+import Market from './market'
+import About from './about'
+import Details from './details'
+import NotFound from './NotFound'
+
 import './index.scss'
+
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <main>
+        <div className="wrapper">
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Market} />
+            <Route path="/about" component={About} />
+            <Route path="/:symbol" component={Details} />
+            <Route component={NotFound} />
+          </Switch>
+          <Footer />
+        </div>
+      </main>
     </ConnectedRouter>
   </Provider>,
   document.querySelector('#root')
