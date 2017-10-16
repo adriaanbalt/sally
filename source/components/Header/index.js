e52cc4f3338b2a70bb0d2e8568816c945e9dbac8
adriaanbalt <adriaan@balt.us>
2017-10-16 16:38:32 -0400

    updating header styles and markup

--- source/components/Header/index.js
+++ source/components/Header/index.js
@@ -1,11 +1,17 @@
+import './index.scss'
 import React from 'react';
 import { Route, Link } from 'react-router-dom'
 
 const Header = () => (
   <header>
-    <img className="logo" src="/logo.png" />
-    <h1>Sally</h1>
-    <Link to="/about">About</Link>
+    <div className="container-logo">
+      <Link to="/" className="anchor-home"><img className="logo" src="/logo.png" /></Link>
+      <h1>Sally.Exchange</h1>
+    </div>
+    <nav>
+      <Link to="/">Market</Link>
+      <Link to="/about">About</Link>
+    </nav>
   </header>
 )
 
