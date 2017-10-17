5e35c94aa3ce0441bd61cd4c6f5f2c85860c9a6a
adriaanbalt <adriaan@balt.us>
2017-10-17 15:20:06 -0400

    adding prod task to run node server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const   express = require('express'),
+        path = require('path'),
+        cookieParser = require('cookie-parser'),
+        bodyParser = require('body-parser'),
+    		http = require('http');
+
+let httpPort = process.env.PORT || 5000
+const app = express()
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+// Process application/json
+app.use(bodyParser.json())
+// Set static folder
+app.use(express.static(path.join(__dirname, '../public')))
+// allows cookie parsing (cookies are simple key value stores in the browser)
+app.use(cookieParser()) 
+
+app.set('port', httpPort )
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
