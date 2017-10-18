13166f57c4ac3507ad9034ed7f972f9690093de4
adriaanbalt <adriaan@balt.us>
2017-10-17 21:18:57 -0400

    updating the styles for the deatils page

--- source/details/index.js
+++ source/details/index.js
@@ -4,6 +4,8 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
 import { Link } from 'react-router-dom'
+import { round } from 'lodash'
+
 
 import {
   setSummaryBySymbol
@@ -17,22 +19,19 @@ class Details extends Component {
   render() {
     console.log('Details render', this.props )
     return(
-      <section id="details">
-        <Link to="/">back</Link>
+      <section id='details'>
+        <Link to='/'>back</Link>
+        <h1 className='exchange-name'>{ this.props.match.params.symbol }</h1>
         {
           this.props.coin
           &&
-          <h1>{ this.props.match.params.symbol }</h1>
-        }
-        {
-          this.props.coin
-          &&
-          <h2>Current Price: { this.props.coin.currentPrice }</h2>
-        }
-        {
-          this.props.coin
-          &&
-          <h2>24 Hour Percentage: { this.props.coin.percentage }</h2>
+          <div className='stats'>
+            <p>Current Price</p>
+            <h2>{ this.props.coin.currentPrice }</h2>
+            <p>24 Hour Percentage</p>
+            <h2>{this.props.coin.percentage}</h2>
+            <h2>{`${this.props.coin.percentage} ${round(this.props.coin.percentage, 2)}%`}</h2>
+          </div>
         }
       </section>
     )
