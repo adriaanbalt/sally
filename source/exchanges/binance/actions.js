41abf03ab7e433bd0898ce3e70eddaacee470c9e
adriaanbalt <adriaan@balt.us>
2017-12-02 16:13:55 -0500

    adding binance exchange gs

---
+++ source/exchanges/binance/actions.js
@@ -0,0 +1,49 @@
+import { get } from '../../API'
+export const CHANGE_VIEW = 'CHANGE_VIEW'
+export const SELL = 'SELL'
+export const BUY = 'BUY'
+export const BINANCE = 'BINANCE'
+
+export const getMarket = () => async ( dispatch, getState ) => {
+  let res = await get('/summary/crypto')
+  dispatch({
+    type: BINANCE,
+    data: res.body
+  })
+}
+
+export const buy = ( amount, type ) => ( dispatch, getState ) => {
+  dispatch({
+    type: BUY,
+    amount,
+    conversion
+  })
+}
+
+export const sell = () => ( dispatch, getState ) => {
+  dispatch({
+    type: SELL,
+    amount,
+    conversion
+  })
+}
+
+export const changeViews = () => ( dispatch, getState ) => {
+  const reducer = getState().binanceReducer;
+  // TODO > handle the quantity view manipulation
+  const previousQuantityView = reducer.views.current
+  let currentQuantityViewIndex = 0
+  reducer.views.options.map( (item, index) => {
+    if ( item == previousQuantityView ) {
+      currentQuantityViewIndex = index+1
+      return
+    }
+  })
+  if ( currentQuantityViewIndex > reducer.views.options.length-1 ) {
+    currentQuantityViewIndex = 0;
+  }
+  dispatch({
+    type: CHANGE_VIEW,
+    currentQuantityView: reducer.views.options[currentQuantityViewIndex]
+  })
+}
