2c7d6194e9cd2d5f82d33635f02150bf48688743
adriaanbalt <adriaan@balt.us>
2017-12-02 16:15:03 -0500

    refactor API to have various functions to hold the endpoint names

--- source/API.js
+++ source/API.js
@@ -6,7 +6,6 @@ import Constants from './constants'
 const apiRequest = o => {
   let endpoint = o.route
   // console.log( 'apiRequest', o)
-  o.params = o.params || [];
   //if(o.body && ['get','del'].indexOf(o.method)!==-1) throw new Error("get and delete requests can't have a body with superagent library");
   o.body = o.body || {};
   o.headers = o.headers || {'Accept': 'application/json'}; // default to 'Accept': 'application/json' header
@@ -22,6 +21,7 @@ const apiRequest = o => {
   });
 }
 
-export const getProd = (route, options) => apiRequest(Object.assign({}, options, { verb: 'GET', route: `${Constants.API}/${Constants.API_VERSION}${route}` }))
-export const getWWW = (route, options) => apiRequest(Object.assign({}, options, { verb: 'GET', route: `${Constants.API_WWW}/${Constants.API_WWW_VERSION}${route}` }))
-export const get = (route, options) => apiRequest(Object.assign({}, options, { verb: 'GET', route }))
+export const getSummaryBySymbol = ({ symbol }) => apiRequest(Object.assign({}, { verb: 'GET', route: `${Constants.API}/1/summary/crypto/${symbol}` }))
+export const getRecordsBySymbol = ({ symbol, period, length }) => apiRequest(Object.assign({}, { verb: 'GET', route: `${Constants.API}/2/records/${ symbol }?period=${ period }&length=${ length }` }))
+export const getSummary = () => apiRequest(Object.assign({}, { verb: 'GET', route: `${Constants.API}/2/summary/crypto` }))
+export const getAutomationPreviewBySymbol = ({ symbol, risk }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://d1b0a1df.ngrok.io/api/1/automation`, body: { symbol, risk } }))
