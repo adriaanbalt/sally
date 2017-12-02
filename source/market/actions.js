3f716aea8cb5fdbe02fa28c786d2b8752660d605
adriaanbalt <adriaan@balt.us>
2017-12-02 16:25:34 -0500

    remove getprod

--- source/market/actions.js
+++ source/market/actions.js
@@ -1,13 +1,13 @@
-import { getProd } from '../API'
+import { getSummary } from '../API'
 
 export const SET_MARKET = 'SET_MARKET'
 
 export const getMarket = () => async ( dispatch, getState ) => {
-  let res = await getProd(`/summary/crypto`)
-  console.log( 'getMarket')
+  let res = await getSummary()
   dispatch({
     type: SET_MARKET,
     gemini: res.body.filter( item => item.symbol.indexOf('GEMINI') > -1 ),
+    binance: res.body.filter( item => item.symbol.indexOf('BINANCE') > -1 ),
     bittrex: res.body.filter( item => item.symbol.indexOf('BITTREX') > -1 ),
     poloniex: res.body.filter( item => item.symbol.indexOf('POLONIEX') > -1 ),
     winkdex: res.body.filter( item => item.symbol.indexOf('WINKDEX') > -1 ),
