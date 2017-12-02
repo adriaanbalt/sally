a506475ed8a91aadc6e4accb5b70e994b77da0fa
adriaanbalt <adriaan@balt.us>
2017-12-02 16:15:42 -0500

    market takes binance

--- source/market/index.js
+++ source/market/index.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import classNames from 'classnames'
 
 import Gemini from '../exchanges/gemini'
+import Binance from '../exchanges/binance'
 import Bittrex from '../exchanges/bittrex'
 import Poloniex from '../exchanges/poloniex'
 import Winkdex from '../exchanges/winkdex'
@@ -21,6 +22,11 @@ class Market extends Component {
           this.props.whichExchange == "gemini"
           &&
           <Gemini data={this.props.gemini}/>
+        }
+        {
+          this.props.whichExchange == "binance"
+          &&
+          <Binance data={this.props.binance}/>
         }
         {
           this.props.whichExchange == "bittrex"
@@ -50,6 +56,7 @@ class Market extends Component {
 const mapStateToProps = state => ({
   isLoading: state.appReducer.isLoading,
   gemini: state.appReducer.gemini,
+  binance: state.appReducer.binance,
   bittrex: state.appReducer.bittrex,
   poloniex: state.appReducer.poloniex,
   winkdex: state.appReducer.winkdex,
