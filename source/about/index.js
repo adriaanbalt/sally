d7c9735be654d2a3abf0a6ad49394ed80c883726
adriaanbalt <adriaan@balt.us>
2017-12-05 00:34:11 -0500

    updating paddings

--- source/about/index.js
+++ source/about/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => (
-  <div>
+  <div className='page page-about'>
     <p>Sally is a crypto data aggregator that is connected to Bloo</p>
   </div>
 )
