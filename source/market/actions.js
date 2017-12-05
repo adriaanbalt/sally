a76d5089d29233b97c55a97d21c52f027bb760ad
adriaanbalt <adriaan@balt.us>
2017-12-05 00:27:24 -0500

    updating model for coins

--- source/market/actions.js
+++ source/market/actions.js
@@ -3,18 +3,13 @@ import { getSummary } from '../API'
 export const SET_MARKET = 'SET_MARKET'
 
 export const getMarket = () => async ( dispatch, getState ) => {
-  console.log("GET MARKET! ACTION ")
   let res = await getSummary()
-
-  res.body = res.body.map(item => Object.assign({ symbol: item.id }, item));
-  console.log('res.body', res.body)
-
   dispatch({
     type: SET_MARKET,
-    gemini: res.body.filter( item => item.symbol.indexOf('GEMINI') > -1 ),
-    binance: res.body.filter( item => item.symbol.indexOf('BINANCE') > -1 ),
-    bittrex: res.body.filter( item => item.symbol.indexOf('BITTREX') > -1 ),
-    poloniex: res.body.filter( item => item.symbol.indexOf('POLONIEX') > -1 ),
-    winkdex: res.body.filter( item => item.symbol.indexOf('WINKDEX') > -1 ),
+    gemini: res.body.filter( item => item.exchange == 'gemini' ),
+    binance: res.body.filter( item => item.exchange == 'binance' ),
+    bittrex: res.body.filter( item => item.exchange == 'bittrex' ),
+    poloniex: res.body.filter( item => item.exchange == 'poloniex' ),
+    winkdex: res.body.filter( item => item.exchange == 'winkdex' ),
   })
 }
