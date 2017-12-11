6bcca5156c89c4b523168b313a4f50e70da299c5
adriaanbalt <adriaan@balt.us>
2017-12-10 23:34:52 -0500

    adding graph fields

--- source/components/Graph/index.js
+++ source/components/Graph/index.js
@@ -2,22 +2,24 @@ import React from 'react'
 import classNames from 'classnames'
 import { Link } from 'react-router-dom'
 
-import Constants from '../../constants'
-import { VictoryChart, VictoryLine, VictoryAxis, VictoryCandlestick, VictoryTheme } from 'victory'
 import MetricsGraphics from 'react-metrics-graphics';
 
 const Graph = (props) => {
-  console.log( 'Graphi', props )
+  console.log('graph', props.coin.graph )
   return (
     <div className='container-graph'>
       <MetricsGraphics
-        title="Downloads"
-        description="This graphic shows a time-series of downloads."
-        data={ props.coin.graph }
-        width={600}
-        height={250}
-        x_accessor="date"
-        y_accessor="value" />
+        data={ props.coin.graph || null }
+        width='600'
+        height='500'
+        x_accessor='date'
+        y_accessor='price'
+        min_y_from_data={true} 
+        lowess={true}
+        show_confidence_band={true}
+        y_extended_ticks={true}
+        yax_count={5}
+      />
     </div>
   )
 }
