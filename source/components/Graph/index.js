439e5573cc53672b2cae7da9893300bcfc58af93
adriaanbalt <adriaan@balt.us>
2017-12-16 21:51:25 -0500

    updating graph styles

--- source/components/Graph/index.js
+++ source/components/Graph/index.js
@@ -10,15 +10,17 @@ const Graph = (props) => {
     <div className='container-graph'>
       <MetricsGraphics
         data={ props.coin.graph || null }
-        width='600'
-        height='500'
+        width='355px'
+        height='300px'
+        top={0}
+        right={0}
         x_accessor='date'
         y_accessor='price'
         min_y_from_data={true} 
-        lowess={true}
+        animate_on_load={true}
         show_confidence_band={true}
         y_extended_ticks={true}
-        yax_count={5}
+        yax_count={10}
       />
     </div>
   )
