09e90be21942ef2a4dbc32da0464dbf9b2d3f044
adriaanbalt <adriaan@balt.us>
2017-12-18 00:17:39 -0500

    adding analyze command to package.json

--- bin/webpack/webpack.dev.config.js
+++ bin/webpack/webpack.dev.config.js
@@ -1,7 +1,6 @@
 // webpack-dev-server -d --hot --config $npm_package_config_build_scripts_dir/webpack/webpack.config.js --watch --progress", 
 const webpack = require('webpack')
 const path = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const ROOT_PATH = path.join(__dirname, '../../' )
 const NODE_MODULES_PATH = path.join(__dirname, '../../node_modules' )
@@ -55,38 +54,7 @@ const config = {
     ]
   },
   plugins: [
-    new webpack.NamedModulesPlugin(),
-    new BundleAnalyzerPlugin({
-      // Can be `server`, `static` or `disabled`.
-      // In `server` mode analyzer will start HTTP server to show bundle report.
-      // In `static` mode single HTML file with bundle report will be generated.
-      // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
-      analyzerMode: 'server',
-      // Host that will be used in `server` mode to start HTTP server.
-      analyzerHost: '127.0.0.1',
-      // Port that will be used in `server` mode to start HTTP server.
-      analyzerPort: 8888,
-      // Path to bundle report file that will be generated in `static` mode.
-      // Relative to bundles output directory.
-      // reportFilename: 'report.html',
-      // Module sizes to show in report by default.
-      // Should be one of `stat`, `parsed` or `gzip`.
-      // See "Definitions" section for more information.
-      defaultSizes: 'parsed',
-      // Automatically open report in default browser
-      openAnalyzer: true,
-      // If `true`, Webpack Stats JSON file will be generated in bundles output directory
-      generateStatsFile: false,
-      // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
-      // Relative to bundles output directory.
-      // statsFilename: 'stats.json',
-      // Options for `stats.toJson()` method.
-      // For example you can exclude sources of your modules from stats file with `source: false` option.
-      // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
-      // statsOptions: null,
-      // Log level. Can be 'info', 'warn', 'error' or 'silent'.
-      logLevel: 'info'
-    })
+    new webpack.NamedModulesPlugin()
   ],
   devServer: {
     contentBase: DIST_PATH,
@@ -95,7 +63,7 @@ const config = {
     disableHostCheck: true,
     historyApiFallback: true
   },
-  devtool: 'eval-source-map'
+  devtool: 'eval'
 }
 
 module.exports = config
