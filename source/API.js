a31fb2ec19357ccf3e1341e7771a046780603e35
adriaanbalt <adriaan@balt.us>
2017-12-18 00:20:16 -0500

    adding login submit API

--- source/API.js
+++ source/API.js
@@ -1,5 +1,4 @@
 import request from 'superagent';
-import Promise from 'bluebird';
 import Constants from './constants'
 
 // console.log ( "Constants", API, API_VERSION )
@@ -15,7 +14,7 @@ const apiRequest = o => {
     .set(o.headers)
     .send(o.body)
     .end((err, res) => {
-      if(err) reject(err);
+      if(err) reject(new Error( res.body.error ) );
       else resolve(res);
     });
   });
@@ -26,3 +25,9 @@ export const getRecordsBySymbol = ({ symbol, period, length }) => apiRequest(Obj
 export const getSummary = () => apiRequest(Object.assign({}, { verb: 'GET', route: `${Constants.API}/2/summary/crypto` }))
 export const getAutomationPreviewBySymbol = ({ symbol, risk }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://d1b0a1df.ngrok.io/api/1/automation`, body: { symbol, risk } }))
 export const getEstimatedReturnsBySymbol = ({symbol}) => apiRequest(Object.assign({}, { verb: 'GET', route: `https://app-production.bloo.financial/indices/nov062017/31/estimated-returns?symbol=${symbol}` }))
+
+export const authSendPhoneNumber = ({ phoneNumber }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://a3feb934.ngrok.io/api/login`, body: { phoneNumber } }))
+export const authSendCode = ({ phoneNumber, code }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://a3feb934.ngrok.io/api/login`, body: { phoneNumber, code } }))
+
+export const getUser = ({ accessToken }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://a3feb934.ngrok.io/api/user`, headers: { 'Access-Token': accessToken } }))
+
