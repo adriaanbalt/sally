3c9644890f1f59d9d208dec9a0d2a9bd0bfc0fc2
adriaanbalt <adriaan@balt.us>
2017-12-20 20:16:49 -0500

    new user url

--- source/API.js
+++ source/API.js
@@ -26,9 +26,9 @@ export const getRecordsBySymbol = ({ symbol, period, length }) => apiRequest(Obj
 export const getSummary = () => apiRequest(Object.assign({}, { verb: 'GET', route: `${Constants.API}/2/summary/crypto` }))
 export const getAutomationPreviewBySymbol = ({ symbol, risk }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://d1b0a1df.ngrok.io/api/1/automation`, body: { symbol, risk } }))
 export const getEstimatedReturnsBySymbol = ({symbol}) => apiRequest(Object.assign({}, { verb: 'GET', route: `https://app-production.bloo.financial/indices/nov062017/31/estimated-returns?symbol=${symbol}` }))
-export const authSendPhoneNumber = ({ phoneNumber }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://a3feb934.ngrok.io/api/login`, body: { phoneNumber } }))
-export const authSendCode = ({ phoneNumber, code }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://a3feb934.ngrok.io/api/login`, body: { phoneNumber, code } }))
-export const getUser = ({ accessToken }) => apiRequest(Object.assign({}, { verb: 'POST', route: `https://a3feb934.ngrok.io/api/user`, headers: { 'Access-Token': accessToken } }))
+export const authSendPhoneNumber = ({ phoneNumber }) => apiRequest(Object.assign({}, { verb: 'POST', route: `automate.bloo.financial/api/login`, body: { phoneNumber } }))
+export const authSendCode = ({ phoneNumber, code }) => apiRequest(Object.assign({}, { verb: 'POST', route: `automate.bloo.financial/api/login`, body: { phoneNumber, code } }))
+export const getUser = ({ accessToken }) => apiRequest(Object.assign({}, { verb: 'POST', route: `automate.bloo.financial/api/user`, headers: { 'Access-Token': accessToken } }))
 
 export const getUserLocalStorage = () => {
   retrieveStoredValue( 'local', 'user' )
